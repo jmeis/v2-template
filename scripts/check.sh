6ef8b45cf853d07b6363f5b1d26a501ca92da196
Vivien Bojtos <Vivien.Bojtos@ibm.com>
2021-04-23 12:17:20 +0200

    feat: move tekton definitions to one-pipeline/compliance-pipelines

--- scripts/check.sh
+++ scripts/check.sh
@@ -5,13 +5,11 @@ set -euo pipefail
 defs=$(mktemp)
 
 echo "Linting CI Pipeline"
-yq r -j .bluemix/ci-tekton.yaml | jq -r '.inputs[] | select(.service=="${TEKTON_CATALOG_REPO}") | "'$COMMON_TEKTON_TASKS_DIR'/" + .path + "/*.yaml"' | tee "$defs"
 yq r -j .bluemix/ci-tekton.yaml | jq -r '.inputs[] | select(.service=="${PIPELINE_REPO}") | .path + "/*.yaml"' | tee -a "$defs"
 
 tekton-lint $(cat "$defs")
 
 echo "Linting PR Pipeline"
-yq r -j .bluemix/pr-tekton.yaml | jq -r '.inputs[] | select(.service=="${TEKTON_CATALOG_REPO}") | "'$COMMON_TEKTON_TASKS_DIR'/" + .path + "/*.yaml"' | tee "$defs"
 yq r -j .bluemix/pr-tekton.yaml | jq -r '.inputs[] | select(.service=="${PIPELINE_REPO}") | .path + "/*.yaml"' | tee -a "$defs"
 
 tekton-lint $(cat "$defs")
